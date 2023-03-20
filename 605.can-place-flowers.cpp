a56f670c4e66d7b9ce08f1ed833dc785e4485bff
Berlin <evan1234.ek@gmail.com>
2023-03-20 21:24:10 +0600

    Solved

---
+++ 605.can-place-flowers.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=605 lang=cpp
+ *
+ * [605] Can Place Flowers
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+
+        if (n == 0)
+        {
+            return true;
+        }
+
+        for (int i = 0; i < flowerbed.size(); i++)
+        {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0))
+            {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
