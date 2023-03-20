8e01d6cf7603a16ea0500483626b70abbd425e55
Berlin <evan1234.ek@gmail.com>
2023-03-20 20:27:05 +0600

    Solved

---
+++ 1.two-sum.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        
+        
+    }
+};
+// @lc code=end
+
