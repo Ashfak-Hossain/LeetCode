449f0c0236242b73283f30271e0ff66fa4044307
Berlin <evan1234.ek@gmail.com>
2023-04-08 14:24:21 +0600

    Solved

---
+++ 1678.goal-parser-interpretation.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=1678 lang=cpp
+ *
+ * [1678] Goal Parser Interpretation
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    string interpret(string command)
+    {
+        string ans;
+        for (int i = 0; i < command.size(); i++)
+        {
+            if (command[i] == 'G')
+                ans.push_back('G');
+            else if (command[i] == '(')
+            {
+                if (command[i + 1] == ')')
+                    ans.push_back('o');
+                else
+                {
+                    ans.push_back('a');
+                    ans.push_back('l');
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
