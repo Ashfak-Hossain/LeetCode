c968af2d5a527ed9d430a25940e6c2a9aa41c1b7
Berlin <evan1234.ek@gmail.com>
2023-04-19 19:22:32 +0600

    solved

--- 290.word-pattern.cpp
+++ 290.word-pattern.cpp
@@ -10,52 +10,29 @@ class Solution
 public:
     bool wordPattern(string pattern, string s)
     {
-        unordered_map<char, string> mp;
-        unordered_map<string, bool> visited;
-        vector<string> wordList;
 
-        // stringstream s
-        stringstream st(s);
-
+        vector<string> v;
         string word;
-        int count = 0;
+        stringstream iss(s);
+        while (iss >> word)
+            v.push_back(word);
 
-        while (st >> word)
-        {
-            count++;
-            wordList.push_back(word);
-        }
-
-        if (pattern.size() != count)
-        {
+        if (pattern.size() != v.size())
             return false;
-        }
-        else
+
+        unordered_map<char, string> mp;
+        unordered_map<string, char> m;
+
+        for (int i = 0; i < v.size(); i++)
         {
-            for (int i = 0; i < pattern.size(); i++)
-            {
+            if (mp.find(pattern[i]) != mp.end() && mp[pattern[i]] != v[i])
+                return false;
 
-                char c = pattern[i];
+            else if (m.find(v[i]) != m.end() && m[v[i]] != pattern[i])
+                return false;
 
-                // case1. if not visted for new letter eg: abba s[dog,dog,dog,dog] so dog is already assinged to 'a' so "dog" will be visited before it comes to the letter b.
-                if (mp[c] == "" && visited[wordList[i]] == true)
-                {
-                    return false;
-                }
-
-                // case 2 .if not visited we will make it visited and assign value as v[i]
-                else if (mp[c] == "")
-                {
-                    mp[c] = wordList[i];
-                    visited[wordList[i]] = true;
-                }
-                else
-                {
-                    // case 3. if value is already assigned so not equal to the v[i].
-                    if (mp[c] != wordList[i])
-                        return false;
-                }
-            }
+            mp[pattern[i]] = v[i];
+            m[v[i]] = pattern[i];
         }
 
         return true;
