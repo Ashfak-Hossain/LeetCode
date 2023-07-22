c21da96dd37b8666bffb97e8ee319c6ebf57a787
Berlin <evan1234.ek@gmail.com>
2023-07-22 10:57:54 +0600

    Solved

---
+++ 1071.greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1071 lang=cpp
+ *
+ * [1071] Greatest Common Divisor of Strings
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    string gcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+            return "";
+        return str1.substr(0, gcd(str1.size(), str2.size()));
+    }
+};
+// @lc code=end
