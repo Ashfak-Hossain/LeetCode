f88e77da03a7311d109c2b9b6009ad77b18e5a3d
Berlin <evan1234.ek@gmail.com>
2023-10-11 19:37:10 +0600

    solved

---
+++ 189. Rotate Array.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        k = k % n;
+
+        if (k > 0)
+        {
+            vector<int> tmp(k);
+
+            for (int i = n - k; i < n; i++)
+            {
+                tmp[i - (n - k)] = nums[i];
+            }
+
+            for (int i = n - k - 1; i >= 0; i--)
+            {
+                nums[i + k] = nums[i];
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                nums[i] = tmp[i];
+            }
+        }
+    }
+};
