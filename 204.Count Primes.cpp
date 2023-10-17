9bb735803e9899fda7e39d06e6445228eaff3bbd
Berlin <evan1234.ek@gmail.com>
2023-10-18 02:28:22 +0600

    solved

---
+++ 204.Count Primes.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        if (n <= 2)
+        {
+            return 0;
+        }
+
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i <= sqrt(n); i++)
+        {
+            if (isPrime[i])
+            {
+                for (int j = i * i; j < n; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++)
+        {
+            if (isPrime[i])
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
